e9eaf5f777a556af647f462b09ce58455dd10f32
macbook <macbook@MacBook-Pro-macbook.local>
2025-02-10 19:48:24 +0100

    test

---
+++ src/main/java/com/example/tspspringback/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.tspspringback.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")  // Apply to all endpoints under "/api"
+                .allowedOrigins("http://localhost:5173")  // Allow requests from this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow these HTTP methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true);  // Allow cookies to be sent with the request
+    }
+}
+
