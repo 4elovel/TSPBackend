9ef12e7952753558675d5388d715b3317efa063f
macbook <macbook@MacBook-Pro-macbook.local>
2025-02-10 19:58:17 +0100

    123

--- src/main/java/com/example/tspspringback/controller/MainController.java
+++ src/main/java/com/example/tspspringback/controller/MainController.java
@@ -4,15 +4,14 @@ import com.example.tspspringback.entity.Coordinate;
 import com.example.tspspringback.service.AlgorithmsService;
 import java.util.List;
 import java.util.Map;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
 public class MainController {
 
     private final List<Coordinate> mockData = List.of(
@@ -35,7 +34,7 @@ public class MainController {
         return mockData;
     }
 
-    @GetMapping("/nearestNeighbor")
+    @PostMapping("/nearestNeighbor")
     public List<Coordinate> getNearestNeighbor(@RequestBody List<Coordinate> coordinates) {
         return algorithmsService.getNearestNeighbor(coordinates);
     }
